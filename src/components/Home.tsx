457ddbf39981326302f042341c95bf2a59496402
bengquist <engquist_blake@yahoo.com>
2019-05-26 23:20:13 -0500

    ts success

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-  return <h1>Home</h1>;
+  return <div>I am Home!</div>;
 }
 
 export default Home;
---
+++ webpack.server.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+
+  entry: "./src/index.tsx",
+
+  devtool: "inline-source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
